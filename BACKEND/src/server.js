a2172a34fe43ca0219c8d8b036dcb077110b7c68
fengliuz <fengliuzng4a@gmail.com>
2026-03-08 17:03:05 +0700

    prepared deploy

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -1,28 +1,35 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors"
+import cors from "cors";
 import NoteRoute from "./Routes/NoteRoute.js";
 import { connectDb } from "./config/db.js";
 import rateLimiter from "./middleware/RateLimiter.js";
+import path from "path";
 dotenv.config();
 const app = express();
-
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 // middleware
-app.use(cors(
- { origin:"http://localhost:5173"}
-))
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 app.use(express.json());
 app.use(rateLimiter);
 // simple custon middleware
 // app.use((req,res,next)=>{
-    //     console.log(`request method is ${req.method} and the uri is ${req.url}`);
-    //     next()
-    // })
-    
-    app.use("/api/notes", NoteRoute);
-    const PORT = process.env.PORT;
-    connectDb().then(() => {
-      app.listen(PORT, () => {
-        console.log("Server run at PORT:", PORT);
-      });
-    });
+//     console.log(`request method is ${req.method} and the uri is ${req.url}`);
+//     next()
+// })
+
+app.use("/api/notes", NoteRoute);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../FRONTEND", "dist", "index.html"));
+  });
+}
+connectDb().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server run at PORT:", PORT);
+  });
+});
--- FRONTEND/src/lib/axios.js
+++ FRONTEND/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios"
+const BASEURL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
 const api = axios.create({
-    baseURL:"http://localhost:5001/api",
+    baseURL:BASEURL,
     headers:{
         Accept:"application/json"
     }
